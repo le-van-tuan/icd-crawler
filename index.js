9ec3fc00e8ce91598a3d0ddda1ca75c0ead2768b
Le Van Tuan <levantuan.itvn@gmail.com>
2024-11-06 16:54:08 +0700

    crawling

--- index.js
+++ index.js
@@ -2,30 +2,165 @@ const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 const Excel = require('exceljs');
 
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+function getMatches(string, regex, index) {
+    index || (index = 1);
+    var matches = [];
+    var match;
+    while ((match = regex.exec(string))) {
+        matches.push(match[index]);
+    }
+    return matches;
+};
+
 (async () => {
-	try {
-		console.log("=======> Opening Browser...");
-		let icdResults = [];
-		let groups = [];
+    console.log("=======> Opening Browser...");
+    let itemIdCounter = 1;
+    let icdItems = [];
 
-		const browser = await puppeteer.launch({headless: false});
-		const page = await browser.newPage();
-		await page.goto("https://icd.kcb.vn/icd-10/icd10-dual");
+    let groupIdCounter = 1;
+    let groups = [];
+    
 
-		
+    const browser = await puppeteer.launch({
+        headless: false,
+        ignoreHTTPSErrors: true,
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        args: [`--window-size=1920,1080`],
+    });
+    const page = await browser.newPage();
+    await page.goto("https://icd.kcb.vn/icd-10/icd10-dual");
+    await page.waitForNetworkIdle();
 
-	    console.log("=======> Total results: " + icdResults.length);
-		console.log("=======> Total groups: " + groups.length);
-		// await browser.close();
-		console.log("=======> DONE <=========");
-	} catch (e) {
-		console.log("========== FAILED =========", e);
-	}
+    /**
+     * Root Level
+     */
+    const rootLevelSelector = "div.card-body > .cdk-tree > .mat-nested-tree-node";
+    const chapters = await page.$$(rootLevelSelector);
+    await page.waitForNetworkIdle();
+    await sleep(1000);
+
+    for (let index = 1; index < chapters.length; index++) {
+        let chapter = chapters[index];
+
+        let span = await chapter.$("span.cursor-pointer");
+        await page.evaluate((element) => {
+            element.click();
+        }, span);
+        console.log("chapter clicked");
+        await page.waitForNetworkIdle();
+
+        let title = await page.evaluate((el) => el.textContent, span);
+        console.log("=> crawling at: " + title.trim());
+        let regex = /\((.*)\)\s*(.*)/g;
+        let code = getMatches(title, regex, 1)[0];
+
+        let group = {
+            id: groupIdCounter,
+            name: getMatches(title, regex, 2)[0].trim(),
+            startCode: code.split("-")[0],
+            endCode: code.split("-")[1],
+            parentId: null,
+            system: true,
+        };
+        groups.push(group);
+        groupIdCounter += 1;
+
+        let expandButton = await chapter.$$("button.mat-focus-indicator");
+        await page.evaluate((element) => {
+            element.click();
+        }, expandButton[0]);
+        await page.waitForNetworkIdle();
+        await sleep(1000);
+
+        /**
+         * Second Level
+         */
+        let secondLevelSelector = `${rootLevelSelector}:nth-child(${index + 1}) > div[role='group'] > .mat-nested-tree-node`;
+        let secondLevelItems = await page.$$(secondLevelSelector);
+        for (let secondLevelIndex = 0; secondLevelIndex < secondLevelItems.length; secondLevelIndex++) {
+            let secondLevelElement = secondLevelItems[secondLevelIndex];
+            let span = await secondLevelElement.$("span.cursor-pointer");
+            await page.evaluate((element) => {
+                element.click();
+            }, span);
+            console.log("click me...");
+            await page.waitForNetworkIdle();
+            await page.waitForSelector("div#detail", {visible: true}, 0);
+
+            const detailContent = await page.$(`div#detail`);
+            const dContentHtml = await page.evaluate((body) => body.innerHTML, detailContent);
+            const $ = await cheerio.load(dContentHtml, null, false);
+
+            let startRowIndex = 2;
+            let rows = $("div.row-layout");
+            if (rows.length < 3) {
+                let span = await secondLevelElement.$("span.cursor-pointer");
+                let title = await page.evaluate((el) => el.textContent, span);
+                console.log();
+                console.error("=== WE SHOULD NEVER GET HERE ===");
+                console.log("AT:", title);
+                console.log();
+                continue;
+            }
+
+            let secondLevelGroup = null; 
+            for (let i = startRowIndex; i < rows.length; i++) {
+                let element = rows[i];
+                let clCode = await $(element).find(".column-code div.content").first().text();
+
+                let columnContent = await $(element).find(".column-content").first().children();
+                let vnText = await $(columnContent[0]).find("div.hover-container h2").first().text();
+                let enText = await $(columnContent[1]).find("div.hover-container h2").first().text();
+
+                if (i == startRowIndex) {
+                    secondLevelGroup = {
+                        id: groupIdCounter,
+                        name: vnText.trim(),
+                        startCode: clCode.trim().split("-")[0],
+                        endCode: clCode.trim().split("-")[1],
+                        parentId: group.id,
+                        system: true,
+                    };
+                    groups.push(secondLevelGroup);
+                    groupIdCounter += 1;
+                } else {
+                    let vnText = await $(columnContent[0]).find("div.hover-container div.content").first().text();
+                    let enText = await $(columnContent[1]).find("div.hover-container div.content").first().text();
+                    let icdItem = {
+                        id: itemIdCounter,
+                        vnName: vnText.trim(),
+                        enName: enText.trim(),
+                        code: clCode.trim(),
+                        groupId: secondLevelGroup.id,
+                        system: true,
+                    };
+                    icdItems.push(icdItem);
+                    itemIdCounter += 1;
+                }
+            }
+        }
+
+        if (index == 3) break;
+    }
+
+    console.log("=======> Total items: " + icdItems.length);
+    console.log("=======> Total groups: " + groups.length);
+    // console.log("=========== GROUPS ==========");
+    // console.log(groups);
+    // console.log("=========== ITEMS ==========");
+    // console.log(icdItems);
+    
+    await exportResults(icdItems, groups);
+    // await browser.close();
+    console.log("=======> DONE <> NICK NỢ 1 THÙNG BIA =========");
 })();
-
-const waitUntilContentLoaded = async (page) => {
-	return await page.waitForSelector('#divMain > div > div.row.form-inline > div.col-xs-8 > div.page-refresh', {hidden : true}, 0);
-}
 
 const exportResults = async (icdResults, groups) => {
 	try {
@@ -33,13 +168,11 @@ const exportResults = async (icdResults, groups) => {
 		let workbook = new Excel.Workbook();
 		let worksheet = workbook.addWorksheet('Ma ICD');
 		worksheet.columns = [
-			{header: 'Mã', key: 'id', width: 10},
-			{header: 'Tên', key: 'name', width: 60},
-			{header: 'Tên Tiếng Anh', key: 'engName', width: 60},
-			{header: 'Tên Nhóm', key: 'groupName', width: 60},
-			{header: 'Mô Tả', key: 'desc', width: 100},
-			{header: 'Mô Tả Tiếng Anh', key: 'engDesc', width: 100}
-		];
+            {header: "Mã", key: "code", width: 10},
+            {header: "Tên", key: "vnName", width: 60},
+            {header: "Tên Tiếng Anh", key: "enName", width: 60},
+            {header: "Thuộc Nhóm", key: "groupId", width: 60}
+        ];
 		icdResults.forEach((e, index) => {
 			worksheet.addRow({
 				...e
@@ -51,13 +184,11 @@ const exportResults = async (icdResults, groups) => {
 
 		let groupsWs = workbook.addWorksheet('Nhom');
 		groupsWs.columns = [
-			{header: 'Tên Nhóm', key: 'name', width: 60},
-			{header: 'Tên Nhóm Tiếng Anh', key: 'enName', width: 60},
-			{header: 'Nhóm Cha', key: 'parent', width: 60},
-			{header: 'Nhóm Cha Tiếng Anh', key: 'enParent', width: 60},
-			{header: 'Mã Bắt Đầu', key: 'startID', width: 50},
-			{header: 'Mã Kết Thúc', key: 'endID', width: 50}
-		];
+            {header: "Tên Nhóm", key: "name", width: 60},
+            {header: "Mã Bắt Đầu", key: "startCode", width: 50},
+            {header: "Mã Kết Thúc", key: "endCode", width: 50},
+            {header: "Nhóm Cha", key: "parentId", width: 60},
+        ];
 		groups.forEach((e, index) => {
 			groupsWs.addRow({
 				...e
---
+++ index_tmp.js
@@ -0,0 +1,157 @@
+const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
+const Excel = require('exceljs');
+
+(async () => {
+	try {
+		console.log("=======> Opening Browser...");
+		let icdResults = [];
+		let groups = [];
+
+		const browser = await puppeteer.launch({headless: false});
+		const page = await browser.newPage();
+		await page.goto('http://icd.kcb.vn/');
+
+		const chapters = await page.$$('#uLeftMenu > ul > li');
+	    for (let i = 0, length = chapters.length; i < length; i++) {
+	        let dropDownMenu = await page.$$(`#uLeftMenu > ul > li:nth-child(${i + 1}) > div > i`);
+	        await dropDownMenu[0].click();
+	        await page.waitForSelector(`#uLeftMenu > ul > li:nth-child(${i + 1}) > div > i.glyphicon-chevron-down`);
+
+	        const chapterMenu = await page.$$(`#uLeftMenu > ul > li:nth-child(${i + 1}) > div > div`);
+	        await chapterMenu[0].click();
+			await waitUntilContentLoaded(page);
+
+			const dContent = await page.$(`#dContent`);
+			const dContentHtml = await page.evaluate(body => body.innerHTML, dContent);
+			const $dct = cheerio.load(dContentHtml);
+
+			let chapterHeaders = $dct(".chapter-header").children();
+			let vnHeader = $dct(chapterHeaders[0]).html().split("<br>")[1];
+			let enHeader = $dct(chapterHeaders[1]).html().split("<br>")[1];
+			let startID = $dct(chapterHeaders[0]).html().split("<br>")[2].split("-")[0].replace("(", "");
+			let endID = $dct(chapterHeaders[0]).html().split("<br>")[2].split("-")[1].replace(")", "");
+			groups.push({name: vnHeader, enName: enHeader, parent: "", enParent: "", startID: startID, endID: endID});
+
+			const menuBlock = await page.$(`#uLeftMenu > ul > li:nth-child(${i + 1}) > div`);
+			const menuHtml = await page.evaluate(body => body.innerHTML, menuBlock);
+	        const $ = cheerio.load(menuHtml);
+	        let title = $("a").text();
+	        console.log("=======> Begin crawl: " + title);
+
+	        await waitUntilContentLoaded(page);
+
+	        const subChapters = await page.$$(`#uLeftMenu > ul > li:nth-child(${i + 1}) > ul > li`);
+			for (let subChapterElement of subChapters) {
+			    await subChapterElement.click();
+			    await waitUntilContentLoaded(page);
+
+			    const content = await page.content();
+			    const $ = cheerio.load(content);
+
+				let rows = $('div#dContent').children();
+				let groupHeader = $('div#dContent').children(".group-header");
+				groups.push({name: $(groupHeader).children("div").first().text(), enName: $(groupHeader).children("div").last().text(), parent: vnHeader, enParent: enHeader, startID: $(groupHeader).attr("item-id").split("-")[0], endID: $(groupHeader).attr("item-id").split("-")[1]});
+
+			    let id = "";
+			    let name = "";
+			    let engName = "";
+			    let groupName = $('div#dContent').children(".group-header").children("div").first().text();
+			    let desc = "";
+			    let engDesc = "";
+
+			    for (let i = 0; i < rows.length; i++) {
+			    	let element = rows[i];
+			    	let clazzName = await $(element).attr("class");
+			    	if (clazzName.includes("line-item")) {
+			    		if (id) {
+			    			icdResults.push({id, name, engName, groupName, desc, engDesc});
+			    		}
+			    		id = $(element).children(clazzName.includes("group") ? ".type-header" : ".item-header").text();
+			    		$(element).find(".item-name").each((index, el) => {
+			    			let value = $(el).text();
+			    			if ($(el).attr("style").includes("left")) {
+			    				name = value;
+			    			} else {
+			    				engName = value;
+			    			}
+			    		});
+			    	} else if (clazzName.includes("item-detail")) {
+			    		$(element).find(".item-include").each((index, el) => {
+			    			let value = $(el).text();
+			    			if ($(el).attr("style") && $(el).attr("style").includes("left")) {
+			    				desc = value;
+			    			} else if ($(el).attr("style") && $(el).attr("style").includes("right")) {
+			    				engDesc = value;
+			    			} else {
+			    				desc = value;
+			    			}
+			    		});
+			    	}
+			    	if ((i == (rows.length - 1))) {
+			    		icdResults.push({id, name, engName, groupName, desc, engDesc});
+			    	};
+			    }
+			}
+			console.log("=======> Finished crawl: " + title);
+	    }
+	    console.log("=======> Total results: " + icdResults.length);
+		console.log("=======> Total groups: " + groups.length);
+	    await exportResults(icdResults, groups);
+		await browser.close();
+		console.log("=======> DONE <=========");
+	} catch (e) {
+		console.log("========== FAILED =========", e);
+	}
+})();
+
+const waitUntilContentLoaded = async (page) => {
+	return await page.waitForSelector('#divMain > div > div.row.form-inline > div.col-xs-8 > div.page-refresh', {hidden : true}, 0);
+}
+
+const exportResults = async (icdResults, groups) => {
+	try {
+		console.log("=======> Begin export results to file...");
+		let workbook = new Excel.Workbook();
+		let worksheet = workbook.addWorksheet('Ma ICD');
+		worksheet.columns = [
+			{header: 'Mã', key: 'id', width: 10},
+			{header: 'Tên', key: 'name', width: 60},
+			{header: 'Tên Tiếng Anh', key: 'engName', width: 60},
+			{header: 'Tên Nhóm', key: 'groupName', width: 60},
+			{header: 'Mô Tả', key: 'desc', width: 100},
+			{header: 'Mô Tả Tiếng Anh', key: 'engDesc', width: 100}
+		];
+		icdResults.forEach((e, index) => {
+			worksheet.addRow({
+				...e
+			});
+		});
+		worksheet.getRow(1).eachCell((cell) => {
+			cell.font = {bold: true};
+		});
+
+		let groupsWs = workbook.addWorksheet('Nhom');
+		groupsWs.columns = [
+			{header: 'Tên Nhóm', key: 'name', width: 60},
+			{header: 'Tên Nhóm Tiếng Anh', key: 'enName', width: 60},
+			{header: 'Nhóm Cha', key: 'parent', width: 60},
+			{header: 'Nhóm Cha Tiếng Anh', key: 'enParent', width: 60},
+			{header: 'Mã Bắt Đầu', key: 'startID', width: 50},
+			{header: 'Mã Kết Thúc', key: 'endID', width: 50}
+		];
+		groups.forEach((e, index) => {
+			groupsWs.addRow({
+				...e
+			});
+		});
+		groupsWs.getRow(1).eachCell((cell) => {
+			cell.font = {bold: true};
+		});
+
+		await workbook.xlsx.writeFile('ICD-results.xlsx');
+		console.log("=======> Finished export result to file...");
+	} catch (e) {
+		console.log("=======> Error while exporting results to file...: ", e);
+	}
+}
